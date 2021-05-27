34df6d4bb6935c2cdcb19a79b6ac17a68250f453
Hendrick201 <81498960+Hendrick201@users.noreply.github.com>
2021-05-26 22:51:54 -0300

    Add files via upload

--- bestburguer.js
+++ bestburguer.js
@@ -1,4 +1,42 @@
+function onclcl()
+{
+    var rua = document.getElementById("rua").value;
+    var bairro = document.getElementById("bairro").value;
+    var numero = document.getElementById("numero").value;
+    var buyButton = document.getElementById("fimPedido");
+    var b1 = document.getElementById("bt1");
+    var b2 = document.getElementById("bt2");
+    var b3 = document.getElementById("bt3");
+    var b4 = document.getElementById("trocoSim");
+    var b5 = document.getElementById("trocoNao");
+    var valorTotal = document.querySelector(".resultFinish").textContent;
+    if(rua != "" && bairro != "" && numero !="" && (b1.checked == true || b2.checked == true || b3.checked == true ))
+    {
+        if (valorTotal > 0)
+        {
+            buyButton.style.color = "white"
+            buyButton.style.backgroundColor = "rgb(245,170,3)"
+            buyButton.disabled = false;
+        }
+        if (valorTotal <= 0)
+        {
+            buyButton.innerHTML = "Adicione itens ao carrinho!"
+        }
+        if ( valorTotal > 0)
+        {
+            buyButton.innerHTML = "FAZER PEDIDO"
+        }
+  
+        if(b1.checked == true)
+        {
+            if(b4.checked == false && b5.checked == false)
+            {
+                buyButton.disabled = true;  
+            }
+        }
+    }
 
+}
 function formaPgt()
 {
     document.getElementById("formaPgt").textContent = event.target.value;
@@ -47,7 +85,7 @@ function buyEnd()
  var rua = document.getElementById("rua").value +", ";
  var bairro = document.getElementById("bairro").value + ". ";
  var numero = document.getElementById("numero").value +", " ;
- var referencia = document.getElementById("referencia").value + ".";  
+ var referencia = document.getElementById("referencia").value;  
  var localização = rua  + numero  + bairro  + referencia ;
 
     document.querySelector("#linktest").href = "https://api.whatsapp.com/send?phone=5534996814576&text=Olá,%20boa%20noite.%20Segue%20o%20meu%20pedido:" +"%0a" +"%0a" + document.getElementById("pedido").textContent +"%0a"  + obs  + "%0a" + "%0a" +"Forma de pagamento:" + "%0a"  + pagamento + "%0a" + "%0a" + document.getElementById("trocos").textContent + "%20"  + "Valor com o qual vai pagar: " + "R$"+valorPago + "%0a"  + "Valor total:" +"%20" + "R$" + valorTotal + "%0a" + "Valor do troco:" + "R$" + valorTroco + "%0a"+ "%0a" + "Localização:" +"%0a" + localização;
@@ -173,17 +211,7 @@ function observationClose()
 }
 function finishListOpen()
 {
-    var valorTotal = document.querySelector(".resultFinish").textContent;
-    var buyButton = document.getElementById("fimPedido");
     document.getElementById("finishDiv").style.display ="block";
-    if(valorTotal == 0)
-    {
-        buyButton.disabled = true;
-    }
-    if(valorTotal >0)
-    {
-        buyButton.disabled = false;
-    }
 }
 function malPassada()
 {
